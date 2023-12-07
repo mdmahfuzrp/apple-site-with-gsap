6adcd50a4b3057fc1623d62d901c36a05ad7b5a1
Mahfuz RP <mahfuzrpsmorg@gmail.com>
2023-12-08 00:56:53 +0600

    gsap animation implement for hero to about

--- script.js
+++ script.js
@@ -11,7 +11,6 @@ let tl = gsap.timeline({
         start: "100% 30px",
         end: "top 30px",
         scrub: 1,
-        markers: true,
     }
 })
 tl.to(".menu-items", {
@@ -20,6 +19,7 @@ tl.to(".menu-items", {
 tl.to("#brand-logo-name", {
     color: '#e4e4e4'
 })
+
 
 // Navbar color change when about section end
 let tl2 = gsap.timeline({
@@ -37,5 +37,37 @@ tl2.to("#brand-logo-name", {
     color: 'black'
 })
 
-
 // Iphone scroll animation from hero section to about section
+let tl3 = gsap.timeline({
+    scrollTrigger: {
+        trigger: "#about",
+        start: "0% 95%",
+        end: "45% 50%",
+        scrub: 1,
+        markers: true,
+    }
+})
+tl3.to("#iphone15", {
+    top: '110%',
+    left: '12%',
+}, 'heroToAbout');
+tl3.to("#wire", {
+    width: '12%',
+    left: '80.5%',
+    top: '115%',
+    rotate: '340deg'
+}, "heroToAbout");
+tl3.to("#apple-logo", {
+    top: '95%',
+    width: '5%',
+    right: '47.5%',
+    zIndex: 2,
+}, "heroToAbout");
+tl3.to("#apple-name", {
+    position: 'fixed',
+    top: '2.7%',
+    fontSize: '16px',
+    letterSpacing: '5px',
+    zIndex: 2,
+    color: "#aaaaaa",
+}, "heroToAbout");
